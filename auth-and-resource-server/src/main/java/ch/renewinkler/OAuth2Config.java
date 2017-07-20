e7e7f70dacf16ea3141476eb63397c876accb9ce
René Winkler <rene.winkler@xovis.com>
2017-07-20 17:51:56 +0200

    different things

---
+++ auth-and-resource-server/src/main/java/ch/renewinkler/OAuth2Config.java
@@ -0,0 +1,53 @@
+package ch.renewinkler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+                .tokenStore(tokenStore());
+    }
+
+    @Bean
+    public ApprovalStore approvalStore() throws Exception {
+        TokenApprovalStore store = new TokenApprovalStore();
+        store.setTokenStore(tokenStore());
+        return store;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("my-trusted-client")
+                .secret("secret")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .scopes("read")
+                .accessTokenValiditySeconds(60);
+    }
+}
--- auth-and-resource-server/src/main/java/ch/renewinkler/SecurityConfig.java
+++ auth-and-resource-server/src/main/java/ch/renewinkler/SecurityConfig.java
@@ -1,37 +1,58 @@
 package ch.renewinkler;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .formLogin()
+
+////        http.requestMatchers().antMatchers("/api/**").and().authorizeRequests()
+////                .antMatchers("/api/**").permitAll();
+//
+////        http
+////                .formLogin()
+////                .and()
+////                .authorizeRequests()
+////                //.antMatchers("/hello").permitAll()
+////                //.anyRequest().authenticated()
+////                .anyRequest().permitAll();
+//
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().permitAll()
                 .and()
-                .httpBasic().disable()
-                //.authorizeRequests()
-                //.antMatchers("/hello").hasRole("TRUSTED_CLIENT")
-                //.anyRequest().authenticated()
-                //.anyRequest().permitAll()
-                //.and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+                .formLogin().permitAll();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("admin").password("admin").roles("ADMIN");
-
     }
 
     @Override
