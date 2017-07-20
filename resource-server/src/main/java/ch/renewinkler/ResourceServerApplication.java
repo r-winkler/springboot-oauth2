6f57a7f4c0448278658fc2911e4956dda41aaf50
René Winkler <rene.winkler@xovis.com>
2017-07-20 14:20:02 +0200

    add resource server

--- resource-server/src/main/java/ch/renewinkler/ResourceServerApplication.java
+++ resource-server/src/main/java/ch/renewinkler/ResourceServerApplication.java
@@ -2,17 +2,18 @@ package ch.renewinkler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableResourceServer
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceServerApplication.class, args);
     }
-
 
     @RequestMapping
     public String hello() {
